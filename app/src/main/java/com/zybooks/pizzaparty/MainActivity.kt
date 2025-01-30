16196854ec3d57a6e6d9286e3072b48a2ca0b7f6
badimala218 <badimala@ucsd.edu>
2025-01-30 00:21:15 -0800

    Initial commit of Pizza Party app

---
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.zybooks.pizzaparty
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.ceil
+
+const val SLICES_PER_PIZZA = 8
+
+/**
+ * The main activity of the app
+ *
+ * This class is the main activity of the application.
+ */
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var numAttendEditText: EditText
+    private lateinit var numPizzasTextView: TextView
+    private lateinit var howHungryRadioGroup: RadioGroup
+
+    /**
+     * On creation of MainActivity
+     *
+     * This method runs on the creation of MainActivity. It locates
+     * the UI elements used for application calculations.
+     *
+     * @param savedInstanceState
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        numAttendEditText = findViewById(R.id.num_attend_edit_text)
+        numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
+        howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+    }
+
+    /**
+     * Calculates the number of pizzas, based on slices of pieces
+     * and how hungry they are, and displays in the TextView.
+     *
+     * @param view
+     */
+    fun calculateClick(view: View) {
+        val numAttendStr = numAttendEditText.text.toString()
+        val numAttend = numAttendStr.toInt()
+
+        // Determine how many slices on average each person will eat
+        val slicesPerPerson = when (howHungryRadioGroup.checkedRadioButtonId) {
+            R.id.light_radio_button -> 2
+            R.id.medium_radio_button -> 3
+            else -> 4
+        }
+
+        // Calculate and show the number of pizzas needed
+        val totalPizzas = ceil(numAttend * slicesPerPerson / SLICES_PER_PIZZA.toDouble()).toInt()
+        numPizzasTextView.text = "Total pizzas: $totalPizzas"
+    }
+}
